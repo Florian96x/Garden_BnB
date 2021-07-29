58f7a5bbaf1d91ea15da543a218cafd8ed75ccc3
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-07-29 19:28:54 +0200

    show view created

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,5 @@
 class GardensController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index] # add show to it
+  skip_before_action :authenticate_user!, only: [:index, :show] # add show to it
 
   def index
     @gardens = Garden.all
