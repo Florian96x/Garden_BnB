c8fde65471a03b8cb11627e86196de21a0561733
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-07-31 16:15:32 +0200

    removing range from js

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,6 @@
 // app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
@@ -11,12 +11,12 @@ const initFlatpickr = () => {
   });
 }
 
-const initFlatpickr = () => {
-  flatpickr("#range_start", {
-    altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
-  });
-}
 
-export { initFlatpickr };
+// const initFlatpickr = () => {
+//   flatpickr("#range_start", {
+//     altInput: true,
+//     plugins: [new rangePlugin({ input: "#range_end"})]
+//   });
+// }
+
 export { initFlatpickr };
