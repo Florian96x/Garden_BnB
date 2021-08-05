c98b7b47cc56041a1cfc0736d0a8d2eca71650ac
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-05 20:22:20 +0200

    cloudinary and unsplash images aligned

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2021_08_03_193401) do
-=======
+
 ActiveRecord::Schema.define(version: 2021_08_04_131703) do
 
 
