c66d3118eea622c5347cbdd0bbda5cbbbe4e4e1d
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-08-07 12:53:00 +0200

    fixing status in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_193936) do
+ActiveRecord::Schema.define(version: 2021_08_07_105046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 2021_08_06_193936) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "accepted", default: false
-    t.string "status"
     t.string "qr_code"
+    t.string "status", default: "pending"
     t.index ["garden_id"], name: "index_bookings_on_garden_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
